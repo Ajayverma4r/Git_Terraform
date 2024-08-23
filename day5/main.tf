8d90b5d8154a82dffe3b9dd9614a79d6aebe659e
Ajayverma4r <145029683+Ajayverma4r@users.noreply.github.com>
2024-08-23 20:31:15 +0530

    day5 task lifecycle&statefile completed

--- day5/main.tf
+++ day5/main.tf
@@ -2,6 +2,7 @@
 resource "aws_instance" "name" {
     ami = "ami-02c21308fed24a8ab"
     instance_type = "t2.micro" 
+    availability_zone = "us-east-1f"
     key_name = "test-key"
       tags = {
       Name="test"
